f20f409c7b2b0fdbe5abfb709d13eff5d5062b8c
SmokePemoluxe <alexey1999bittok@gmail.com>
2024-09-10 18:25:49 +0700

    Fix Updater for programm

--- ProgrammUpdater.bat
+++ ProgrammUpdater.bat
@@ -8,6 +8,8 @@ set "customKeyPath=%scriptDir%CustomKey.exe"
 timeout /t 2 /nobreak > nul
 taskkill /F /IM CustomKey.exe 3>nul
 timeout /t 2 /nobreak > nul
+taskkill /F /IM CKinfo_sender.exe 3>nul
+timeout /t 2 /nobreak > nul
 xcopy /s /y "%sourceFolder%\*" "%CD%"
 timeout /t 2 /nobreak > nul
 rd /s /q "%sourceFolder%"
